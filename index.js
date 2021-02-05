94efe51e48b28a7bf853beee58056005c88cf4c9
winningarc <shokim01@gmail.com>
2021-02-06 00:54:56 +0900

    fix db connection check

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-require('dotenv').config();
-const Koa = require('koa');
-const bodyParser = require('koa-body');
-const cors = require('@koa/cors');
-const logger = require('koa-logger');
-const router = require('./src/routes');
-const models = require('./src/database/models/index.js');
-const helmet = require('koa-helmet');
-const swagger = require('koa2-swagger-ui');
-const swaggerDoc = require('./src/utils/swaggerDef.js');
-const { jwtMiddleware } = require('./src/utils');
+require("dotenv").config();
+const Koa = require("koa");
+const bodyParser = require("koa-body");
+const cors = require("@koa/cors");
+const logger = require("koa-logger");
+const router = require("./src/routes");
+const models = require("./src/database/models/index.js");
+const helmet = require("koa-helmet");
+const swagger = require("koa2-swagger-ui");
+const swaggerDoc = require("./src/utils/swaggerDef.js");
+const { jwtMiddleware } = require("./src/utils");
 
 const run = async () => {
-  const app = new Koa();
+    const app = new Koa();
+    try {
+        await models.sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
 
-  models.sequelize
-    .sync()
-    .then(() => {
-      console.log(' DB 연결 성공');
-    })
-    .catch((err) => {
-      console.log('연결 실패');
-      console.log(err);
+    app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
+    app.use(helmet());
+    app.use(logger());
+    app.use(bodyParser());
+    app.use(jwtMiddleware);
+    app.use(router.routes()).use(router.allowedMethods());
+    app.use(
+        swagger({
+            routePrefix: "/swagger",
+            swaggerOptions: {
+                url: "/swagger.json",
+            },
+        })
+    );
+    app.use(swaggerDoc.routes());
+
+    const PORT = 8080;
+
+    app.listen(PORT, () => {
+        console.log(`Server running at port ${PORT}`);
     });
-
-  app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
-  app.use(helmet());
-  app.use(logger());
-  app.use(bodyParser());
-  app.use(jwtMiddleware);
-  app.use(router.routes()).use(router.allowedMethods());
-  app.use(
-    swagger({
-      routePrefix: '/swagger',
-      swaggerOptions: {
-        url: '/swagger.json',
-      },
-    }),
-  );
-  app.use(swaggerDoc.routes());
-
-  const PORT = 8080;
-
-  app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
-  });
 };
 
 run();
